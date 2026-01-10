2f2d4e38bf08416f7a7b8a03eb54e74f4373f9dc
Ajay Kumar <ajaykumarmkaju@gmail.com>
2026-01-10 19:31:52 +0530

    Complete UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
 function App() {
+  const days = [
+    {
+      date: "2 Oct",
+      special: "Gandhi Jayanthi",
+    },
+    {
+      date: "15 Aug",
+      special: "Independence Day",
+    },
+  ];
+
   return (
-    <>
-      <div className="">Public Holidays App</div>
-    </>
+    <div className="paper container container-md margin-top-large">
+      <h1 className="text-center">Public Holidays App</h1>
+
+      <div className="form-group margin-bottom-large">
+        <label htmlFor="countrySelect">Select Country</label>
+        <select id="countrySelect">
+          <option value="1">Option 1</option>
+          <option value="2">Option 2</option>
+          <option value="3">Option 3</option>
+        </select>
+      </div>
+
+      <ul>
+        {days.map((day, index) => (
+          <li className="margin-bottom-small" key={index}>
+            {day.date} {day.special}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
